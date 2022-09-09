0e0cbfa5d7006d817f610e51fc746c44efc3e0a7
hito-kotaro <tohi.work@gmail.com>
2022-09-10 08:51:02 +0900

    tailwind-color設定

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -4,12 +4,12 @@ import { useNavigate } from 'react-router-dom';
 const Login = () => {
   const navigate = useNavigate();
   return (
-    <>
+    <div className="bg-base">
       <div>Login</div>
       <button type="button" onClick={() => navigate('/home')}>
         toHome
       </button>
-    </>
+    </div>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@ module.exports = {
   content: [],
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        success: '#5CD63D',
+        alert: '#F47171',
+        primary: '#FBA305',
+        secondary: '#FBB066',
+        base: '#FFF4EB',
+        text: '#6F7073',
+        // for badge
+        open: '#5CD63D',
+        cancel: '#FFE602',
+        reject: '#F47171',
+      },
+    },
   },
   plugins: [],
 };
