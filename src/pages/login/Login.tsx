01d1faa6fadc17dec1270f24fdb225d478737fe0
hito-kotaro <tohi.work@gmail.com>
2022-09-10 09:44:36 +0900

    login画面着手

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -4,8 +4,11 @@ import { useNavigate } from 'react-router-dom';
 const Login = () => {
   const navigate = useNavigate();
   return (
-    <div className="bg-base">
-      <div>Login</div>
+    <div className="bg-base h-screen">
+      <div className="h-10" />
+      <div className=" h-3/4 w-2/6 bg-white ml-auto mr-10 rounded-xl drop-shadow-lg">
+        <div className="text-center">Logo</div>
+      </div>
       <button type="button" onClick={() => navigate('/home')}>
         toHome
       </button>
