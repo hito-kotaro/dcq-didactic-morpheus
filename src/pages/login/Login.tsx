1ac5ec195c618f68d671f4406b922d9799d05753
hito-kotaro <tohi.work@gmail.com>
2022-09-11 00:08:36 +0900

    loginパネルパーツ配置

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,13 +1,43 @@
+import Button from '@mui/material/Button/Button';
+import TextField from '@mui/material/TextField/TextField';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
   const navigate = useNavigate();
   return (
-    <div className="bg-base h-screen">
+    <div className="">
       <div className="h-10" />
-      <div className=" h-3/4 w-2/6 bg-white ml-auto mr-10 rounded-xl drop-shadow-lg">
+      <div className=" h-3/4 min-h-500 w-2/6 min-w-400 bg-white ml-auto mr-10 rounded-xl drop-shadow-lg">
         <div className="text-center">Logo</div>
+
+        <div className="h-12">
+          <TextField
+            id="outlined-password-input"
+            label="Password"
+            type="password"
+            autoComplete="current-password"
+          />
+        </div>
+        <div className="h-12">
+          <TextField
+            id="outlined-password-input"
+            label="userName"
+            type="text"
+            autoComplete="current-password"
+          />
+        </div>
+        <div className="h-12">
+          <TextField
+            id="outlined-password-input"
+            label="account"
+            type="text"
+            autoComplete="current-password"
+          />
+        </div>
+        <div className="flex justify-end px-5">
+          <Button variant="contained">Login</Button>
+        </div>
       </div>
       <button type="button" onClick={() => navigate('/home')}>
         toHome
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,22 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
     extend: {
+      minHeight: {
+        0: '0',
+        '1/4': '25%',
+        '1/2': '50%',
+        '3/4': '75%',
+        full: '100%',
+        500: '500px',
+      },
+      minWidth: {
+        0: '0',
+        '1/4': '25%',
+        '1/2': '50%',
+        '3/4': '75%',
+        full: '100%',
+        400: '400px',
+      },
       colors: {
         success: '#5CD63D',
         alert: '#F47171',
