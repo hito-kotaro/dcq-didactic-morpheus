fc4316d19bdd0972a88ed7ccee14405aab0e67e2
hito-kotaro <tohi.work@gmail.com>
2022-09-13 21:44:20 +0900

    loginページ調整

---
+++ src/components/org/LoginPanel/LoginPanel.tsx
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import { TextField, Button } from '@mui/material';
+import sampleLogo from '../../../images/sampleLogo.svg';
+import useInputForm from '../../../hooks/InputForm/useInputForm';
+
+const LoginPanel = () => {
+  const [isSignUp, setIsSignUp] = useState(false);
+  const userHandler = useInputForm();
+  const pwdHandler = useInputForm();
+  const tenantHandler = useInputForm();
+
+  return (
+    <div>
+      <div className="h-10" />
+      <div className=" h-3/4 min-h-600 max-h-600  w-2/6 min-w-400 max-w-400 bg-white ml-auto mr-10 rounded-xl drop-shadow-lg">
+        <div className="h-5" />
+
+        <div className="flex justify-center">
+          <img src={sampleLogo} alt="sampleLogo" />
+        </div>
+
+        <div className="h-10" />
+
+        <div className="w-2/3 mx-auto">
+          <div className="">
+            <TextField
+              fullWidth
+              type="text"
+              label={isSignUp ? '新しいテナント名' : 'テナント名'}
+              variant="outlined"
+              onChange={tenantHandler.onChange}
+              value={tenantHandler.value}
+            />
+          </div>
+
+          <div className="h-8" />
+
+          <div className={isSignUp ? 'hidden' : ''}>
+            <TextField
+              fullWidth
+              type="text"
+              label="user"
+              variant="outlined"
+              onChange={userHandler.onChange}
+              value={userHandler.value}
+            />
+          </div>
+
+          <div className="h-8" />
+
+          <div className="">
+            <TextField
+              fullWidth
+              type="password"
+              label={isSignUp ? '新しいテナントのパスワード' : 'パスワード'}
+              variant="outlined"
+              onChange={pwdHandler.onChange}
+              value={pwdHandler.value}
+            />
+          </div>
+          <Button variant="text" onClick={() => setIsSignUp(!isSignUp)}>
+            新しいテナントを作成
+          </Button>
+          <div className="h-10" />
+          <div className="flex justify-center">
+            <Button variant="contained">Login</Button>
+          </div>
+        </div>
+        <div className="h-10" />
+        <Button variant="text">パスワードを忘れましたか？</Button>
+      </div>
+    </div>
+  );
+};
+export default LoginPanel;
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,67 +1,18 @@
-import Button from '@mui/material/Button/Button';
-import TextField from '@mui/material/TextField/TextField';
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import useInputForm from '../../hooks/InputForm/useInputForm';
+import LoginPanel from '../../components/org/LoginPanel/LoginPanel';
 
 const Login = () => {
+  const [isSignUp, setIsSignUp] = useState(false);
   const navigate = useNavigate();
-  const userHandler = useInputForm();
-  const pwdHandler = useInputForm();
-  const tenantHandler = useInputForm();
+
+  const changePanel = () => {
+    setIsSignUp(!isSignUp);
+  };
 
   return (
     <div className="">
-      <div className="h-10" />
-      <div className=" h-3/4 min-h-500 w-2/6 min-w-400 bg-white ml-auto mr-10 rounded-xl drop-shadow-lg">
-        <div className="text-center">Logo</div>
-
-        <div className="h-10" />
-
-        <div className="w-2/3 mx-auto">
-          <div className="">
-            <TextField
-              fullWidth
-              type="text"
-              label="テナント名"
-              variant="outlined"
-              onChange={tenantHandler.onChange}
-              value={tenantHandler.value}
-            />
-          </div>
-
-          <div className="h-8" />
-
-          <div className="">
-            <TextField
-              fullWidth
-              type="text"
-              label="user"
-              variant="outlined"
-              onChange={userHandler.onChange}
-              value={userHandler.value}
-            />
-          </div>
-
-          <div className="h-8" />
-
-          <div className="">
-            <TextField
-              fullWidth
-              type="password"
-              label="password"
-              variant="outlined"
-              onChange={tenantHandler.onChange}
-              value={tenantHandler.value}
-            />
-          </div>
-          <div className="h-10" />
-
-          <div className="flex justify-center">
-            <Button variant="contained">Login</Button>
-          </div>
-        </div>
-      </div>
+      <LoginPanel />
       <button type="button" onClick={() => navigate('/home')}>
         toHome
       </button>
