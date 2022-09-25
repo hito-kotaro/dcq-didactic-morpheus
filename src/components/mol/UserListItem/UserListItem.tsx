af7cc3ca23d4e0eb7fae0dd1c9cef975eddbe819
hito-kotaro <tohi.work@gmail.com>
2022-09-25 10:34:46 +0900

    ユーザリストアイテム配置変更

--- src/components/mol/UserListItem/UserListItem.tsx
+++ src/components/mol/UserListItem/UserListItem.tsx
@@ -1,9 +1,12 @@
 import React, { VFC } from 'react';
-import { Button } from '@mui/material';
+import { Avatar, Button } from '@mui/material';
 import MyAvatar from '../../atoms/MyAvatar/MyAvatar';
 import ScoreIcon from '../../atoms/ScoreIcon/ScoreIcon';
 import { userDataType } from '../../../types/data/userDataType';
 import DcpIcon from '../../atoms/DcpIcon/DcpIcon';
+import MyBadge from '../../atoms/MyBadge/MyBadge';
+import { badgeType } from '../MyBadgeList/myBadgeType';
+import MyBadgeList from '../MyBadgeList/MyBadgeList';
 
 type Props = {
   user: userDataType;
@@ -12,15 +15,28 @@ type Props = {
 
 const UserListItem: VFC<Props> = (props) => {
   const { user, onClick } = props;
+  const badgeList: badgeType[] = [{ bg: '', text: '', content: user.role }];
   return (
-    <div className="flex px-3">
-      <Button fullWidth onClick={() => onClick(user)}>
-        <MyAvatar name={user.name} team={user.team} />
-        <div className="ml-auto">
-          <ScoreIcon icon={<DcpIcon size="sm" />} score={user.point} />
+    <Button fullWidth onClick={() => onClick(user)}>
+      <div className="w-full">
+        <div className="flex px-3">
+          <div>
+            <Avatar />
+          </div>
+          <div className="w-3" />
+          <div className="w-full">
+            <div className="flex">
+              <div className="text-text text-lg font-semibold">{`${user.name} @ ${user.team}`}</div>
+              <div className="ml-auto">
+                <ScoreIcon icon={<DcpIcon size="sm" />} score={user.point} />
+              </div>
+            </div>
+            <div className="h-1" />
+            <MyBadgeList badgeList={badgeList} />
+          </div>
         </div>
-      </Button>
-    </div>
+      </div>
+    </Button>
   );
 };
 
